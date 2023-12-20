5ac11135d0780595c1c2e7c8dee38db0be5b80a5
Roman <rvaiser@technotimes.org>
2023-12-21 00:50:37 +0400

    cool paste shortcut

--- lua/devise/remap.lua
+++ lua/devise/remap.lua
@@ -27,6 +27,6 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 
--- vim.keymap.set("x", "<leader>P", "\"_dP")
+vim.keymap.set("x", "<leader>P", "\"_dP")
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
